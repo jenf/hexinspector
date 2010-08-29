5735cecd37acef2c2385a7f3953d472d186cf8d9
Jennifer Freeman <jen.a.freeman@googlemail.com>
2010-08-30 00:16:34 +0100

    Add some extra neatness.

--- hexfile.rb
+++ hexfile.rb
@@ -22,10 +22,10 @@ class HexInspectorFile
    end
  end
 
- def find_substr(str,offset=0)
+ def find_substr(str,offset=0, minimum=4, stopat=32)
    len=0
    options=nil
-   (offset..str.size-1).each do |x|
+   (offset..(str.size-1)).each do |x|
      char=str[x]
      if @cross.include? char
        if options==nil
@@ -35,27 +35,30 @@ class HexInspectorFile
          options.delete(x) if str==self
        else
          options_shadow = options.dup
-         puts options.inspect
+         #puts options.inspect
          options.each_index do |y|
 #          puts "a . %s %s %s" % [y,char, options[y].inspect]
           options.delete_at(y) unless @cross[char].include? options[y]+x
          end
          if options.size == 0
            options = options_shadow
-           puts "Breaking"
+#           puts "Breaking"
            break
          end
        end
 #       puts "%s %s" % [char, @cross[char].inspect]
      end
 #     puts "Options : %s" % options.inspect
-     len=x
+     len=x-offset
+     break if len>stopat
    end
 
-   puts "Options : len %i : %s" % [len,options.inspect] 
+   puts "Out!"
+#   puts "Options : len %i : %s" % [len,options.inspect] 
    #options.each do |x|
    #  puts @data[x..x+len].inspect
    #end
+   return [nil, len] if len<minimum
    return [options, len]
  end
 
--- hexinspector.rb
+++ hexinspector.rb
@@ -11,9 +11,12 @@ class HexInspector
     @width = 16
     @file = file
     @offset=0
+    Ncurses.start_color()
+    Ncurses.init_pair(1, Ncurses::COLOR_BLUE, Ncurses::COLOR_BLACK);
+    Ncurses.init_pair(2, Ncurses::COLOR_BLACK, Ncurses::COLOR_WHITE);
   end
 
-  def dump_hex(file,lines,offset=0,width=16)
+  def dump_hex(file,lines,offset=0,width=16,startpos=0,curpos=0)
   i=0
   (offset..offset+(lines*width)).step(width) do |x|
     str=""
@@ -23,7 +26,7 @@ class HexInspector
       str << "%02x "% file[x+y] if file[x+y]!=nil
       str << "  " if file[x+y]==nil
     end
-    Ncurses.mvaddstr(i,0,str)
+    Ncurses.mvaddstr(i+startpos,0,str)
     i+=1
   end
   end
@@ -31,14 +34,21 @@ class HexInspector
 
   def show_ruler()
     bit8_val=@file[@offset]
-    Ncurses.mvaddstr(@height-1,0,"Byte %i/0x%08x Val : %03i/0x%02x/0%03o" % [@offset,@offset,bit8_val,bit8_val,bit8_val])
+    @window.attrset(Ncurses.COLOR_PAIR(2))
+    Ncurses.mvaddstr(@height-1,0,"Byte %i/0x%08x Val : %03i/0x%02x/0%03o Substr Match: %i" % [@offset,@offset,bit8_val,bit8_val,bit8_val, @matches.size])
+    @window.attrset(Ncurses.COLOR_PAIR(0))
   end
 
   def show_substrs()
     (matches,len) = @file.find_substr(@file,@offset)
-    Ncurses.mvaddstr(@height-2,0,"Len %i" % len)
+    @matches=matches
+    @matches=[] if @matches==nil
+    (0..(matches.size < 3 ? matches.size-1 : 3)).each do |x|
+     Ncurses.mvaddstr(@height-4+x,0,"Match %i %i" % [matches[x], len])
+    end
   end
   def main_loop
+   @matches=[] if @matches==nil
    while true
     if @window_size_changed
       @height=@window.getmaxy
@@ -47,7 +57,7 @@ class HexInspector
     end
     Ncurses.clear
     dump_hex(@file, @pager_lines, @offset) 
-    show_substrs()
+#    show_substrs()
     show_ruler()
     Ncurses.refresh
 
@@ -62,6 +72,8 @@ class HexInspector
       @offset=0 if @offset<0
      when Ncurses::KEY_DOWN
       @offset+=@width
+     when 's'[0]
+       @offset=@matches[0]
      when Ncurses::KEY_RESIZE
        @window_size_changed=true
     end
