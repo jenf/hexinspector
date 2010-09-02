5fefb4713515db94651cd392476df5e99c40f03b
Jennifer Freeman <jen.a.freeman@googlemail.com>
2010-09-03 00:11:15 +0100

    Add xdiff bpatch applier

---
+++ hexdump.rb
@@ -0,0 +1,21 @@
+#require "hexfile.rb"
+
+#h=open(ARGV[0])
+#file=h.read
+
+def dump_hex(file,offset=0,width=16)
+  str=""
+  i=0
+  (offset..file.size).step(width) do |x|
+    print "%08x " % x
+    (0..width-1).each do |y|
+      print " " if (y % 8)==0
+      print "%02x "% file[x+y] if file[x+y]!=nil
+      print "  " if file[x+y]==nil
+    end
+    puts ""
+  end
+end
+
+#dump_hex(file)
+
